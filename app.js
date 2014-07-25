255a9a780cf9fe8fc8a5873afe2ad0711b908d73
ecsca <ecscaop@gmail.com>
2014-07-25 10:38:54 +0900

    handleDisconnect

--- app.js
+++ app.js
@@ -15,6 +15,40 @@ var mysql = require('mysql');
 //var terminal = require('child_process').exec('python getData.py');
 console.log("terminal started");
 
+
+var db_config = {
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b02535f8bb3e27',
+    port: 3306,
+    password: 'f00599ce453aa27',
+    database: 'heroku_4225976e02f5a12'
+};
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        //console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+/*
 var connection = mysql.createConnection({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b02535f8bb3e27',
@@ -22,7 +56,8 @@ var connection = mysql.createConnection({
     password: 'f00599ce453aa27',
     database: 'heroku_4225976e02f5a12'
 });
-
+*/
+/*
 connection.connect(function (err) {
     if (err) {
         console.error('mysql connection err');
@@ -30,6 +65,7 @@ connection.connect(function (err) {
         throw err;
     }
 });
+*/
 /*
    var Server = mongo.Server,
    Db = mongo.Db,
